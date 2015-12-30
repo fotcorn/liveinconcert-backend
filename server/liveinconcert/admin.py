b0c86ba883d45417dc9d68f58a7df5f4fd3ea0fb
fotcorn <fotcorn@gmail.com>
2015-12-30 18:53:49 +0100

    [Core] initial commit with models & admin config

---
+++ server/liveinconcert/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from models import Artist, ArtistRating, Event, EventRSVP
+
+admin.site.register(Artist)
+admin.site.register(ArtistRating)
+admin.site.register(Event)
+admin.site.register(EventRSVP)
---
+++ server/liveinconcert/models.py
@@ -0,0 +1,63 @@
+from django.conf import settings
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class ArtistRating(models.Model):
+    RATING_LOVE = 1
+    RATING_LIKE = 2
+    RATING_KNOW = 3
+    RATING_NO = 4
+
+    RATING_CHOICES = (
+        (RATING_LOVE, _('Love')),
+        (RATING_LIKE, _('Like')),
+        (RATING_KNOW, _('Know')),
+        (RATING_NO, _('Do not like')),
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    artist = models.ForeignKey(Artist)
+    rating = models.IntegerField(choices=RATING_CHOICES)
+
+    class Meta:
+        ordering = ('artist__name', )
+        unique_together = ('user', 'artist')
+
+    def __unicode__(self):
+        return u'{} - {}'.format(self.artist.name, self.user.get_full_name())
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=500)
+    artist = models.ForeignKey(Artist)
+    location = models.CharField(_('Location'), max_length=500)
+    date_time = models.DateTimeField(_('Date & Time'))
+    bandsintown_id = models.CharField('BandsInTown ID', max_length=100, null=True, blank=True)
+
+    def __unicode__(self):
+        return u'{} {}'.format(self.name, self.date_time)
+
+
+class EventRSVP(models.Model):
+    RSVP_YES = 1
+    RSVP_NO = 2
+    RSVP_UNKNOWN = 3
+
+    RSVP_CHOICES = (
+        (RSVP_YES, _('Yes')),
+        (RSVP_NO, _('No')),
+        (RSVP_UNKNOWN, _('Unknown')),
+    )
+    rsvp = models.IntegerField(choices=RSVP_CHOICES)
+    event = models.ForeignKey(Event)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __unicode__(self):
+        return u'{} - {} - {}'.format(self.event, self.user, self.get_rsvp_display())
