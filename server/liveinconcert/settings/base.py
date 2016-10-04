65a4021d3264d852f70f2bfa7eccf9d2fd0ddae4
fotcorn <fotcorn@gmail.com>
2016-10-04 22:39:43 +0200

    server: fixed static files

--- server/liveinconcert/settings/base.py
+++ server/liveinconcert/settings/base.py
@@ -87,10 +87,6 @@ STATIC_URL = get_env_variable('STATIC_URL', '/static/')
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = get_env_variable('STATIC_ROOT', '/tmp/static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
