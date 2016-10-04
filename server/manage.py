d2c36821cc7cf1f1f47f2551ab0cc97fe0aaf8f4
fotcorn <fotcorn@gmail.com>
2016-10-04 21:36:38 +0200

    server: enhanced settings

--- server/manage.py
+++ server/manage.py
@@ -2,8 +2,13 @@
 import os
 import sys
 
+from liveinconcert import get_project_root_path, import_env_vars
+
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liveinconcert.settings")
+    import_env_vars(os.path.join(get_project_root_path(), 'envdir'))
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liveinconcert.settings.dev")
 
     from django.core.management import execute_from_command_line
 
