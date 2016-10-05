c414ddcca9510988fdc54f19c28345fa996475df
fotcorn <fotcorn@gmail.com>
2016-10-05 09:23:46 +0200

    server: fixed wsgi file

--- server/liveinconcert/wsgi.py
+++ server/liveinconcert/wsgi.py
@@ -4,13 +4,16 @@ WSGI config for liveinconcert project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
 """
 
 import os
 
+from liveinconcert import get_project_root_path, import_env_vars
+
+import_env_vars(os.path.join(get_project_root_path(), 'envdir'))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liveinconcert.settings.base")
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liveinconcert.settings")
-
 application = get_wsgi_application()
