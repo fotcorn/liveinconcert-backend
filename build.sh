28465d98143bcc4cfbf1048a324018ff3eb283fc
fotcorn <fotcorn@gmail.com>
2016-10-22 01:28:56 +0200

    server: updated static files

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ cd client-vue
 npm run build
 cd ..
 cp client-vue/dist/index.html server/frontend/templates/frontend/index.html
+rm -rf server/frontend/static/
 cp -r client-vue/dist/static/ server/frontend/
