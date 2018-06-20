6c9eeab07faeab7a94c4ae5a4924280bf07a7756
fotcorn <fotcorn@gmail.com>
2018-06-20 16:10:26 +0200

    remove old client code

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd client-vue
-npm run build
-cd ..
-cp client-vue/dist/index.html server/frontend/templates/frontend/index.html
-rm -rf server/frontend/static/
-cp -r client-vue/dist/static/ server/frontend/
