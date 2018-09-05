e120e5ff5218267b163a7e5e9a2c999ae7e4a73d
fotcorn <fotcorn@gmail.com>
2018-09-05 10:01:59 +0200

    send notifications from update_rsvp script

--- liveinconcert/management/commands/update_rsvp.py
+++ liveinconcert/management/commands/update_rsvp.py
@@ -1,11 +1,23 @@
+from collections import defaultdict
+
+from django.contrib.auth.models import User
 from django.core.management import BaseCommand
+from django.utils import timezone
 
 from liveinconcert.models import Event, ArtistRating, EventRSVP
+from push.send import send_message
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for event in Event.objects.all():
+        users = defaultdict(lambda: 0)
+        for event in Event.objects.filter(date_time__gt=timezone.now()):
             for rating in event.artist.artistrating_set.exclude(rating=ArtistRating.RATING_NO):
-                EventRSVP.objects.get_or_create(event=event, user=rating.user,
-                                                defaults={'rsvp': EventRSVP.RSVP_UNKNOWN})
+                if not EventRSVP.objects.filter(event=event, user=rating.user).exists():
+                    users[rating.user.pk] += 1
+                    EventRSVP.objects.create(event=event, user=rating.user, rsvp=EventRSVP.RSVP_UNKNOWN)
+
+        for user_pk, count in users.items():
+            user = User.objects.get(pk=user_pk)
+            for token in user.firebasepushtoken_set.all():
+                send_message(token.token, '{} new concerts for you!'.format(count))
