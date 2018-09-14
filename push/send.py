b3fbbbb5767418aed9cd70dc06d465d153f5c01d
fotcorn <fotcorn@gmail.com>
2018-09-14 22:42:26 +0200

    push: remove old/invalid push tokens

--- push/send.py
+++ push/send.py
@@ -1,13 +1,22 @@
 import datetime
 import firebase_admin
 from firebase_admin import messaging, credentials
+from firebase_admin.messaging import ApiCallError
 from django.conf import settings
+from push.models import FirebasePushToken
 
 
-def send_message(registration_token, message):
-    if not send_message.app:
+def send_message_user(user, message):
+    for token in user.firebasepushtoken_set.all().values_list('token', flat=True):
+        message_id = send_message_token(token, message)
+        if not message_id:
+            FirebasePushToken.objects.filter(token=token).delete()
+
+
+def send_message_token(registration_token, message):
+    if not send_message_token.app:
         cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS_PATH)
-        send_message.app = firebase_admin.initialize_app(credential=cred)
+        send_message_token.app = firebase_admin.initialize_app(credential=cred)
     message = messaging.Message(
         notification=messaging.Notification(
             title='Live in Concert',
@@ -21,7 +30,13 @@ def send_message(registration_token, message):
         ),
         token=registration_token
     )
-    print(messaging.send(message))
+
+    message_id = None
+    try:
+        message_id = messaging.send(message)
+    except ApiCallError:
+        pass
+    return message_id
 
 
-send_message.app = None
+send_message_token.app = None
--- push/views.py
+++ push/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 
-from push.send import send_message
+from push.send import send_message_user
 
 
 def send_notification(request):
     sent = []
     if request.method == 'POST' and request.user.is_authenticated:
         text = request.POST['text']
-        for token in request.user.firebasepushtoken_set.all():
-            send_message(token.token, text)
-            sent.append(token.token)
+        send_message_user(request.user, text)
 
     return render(request, 'push/index.html', {'sent': sent})
